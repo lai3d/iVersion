3ecd78beea8739ea253743a0f919b38a26ae293f
Larry Lai <cgpanda.sg@gmail.com>
2016-08-15 15:58:43 +0800

    Check our own app

--- Examples/iPhone Demo/Classes/iVersionAppDelegate.m
+++ Examples/iPhone Demo/Classes/iVersionAppDelegate.m
@@ -25,12 +25,14 @@
     //set the bundle ID. normally you wouldn't need to do this
     //as it is picked up automatically from your Info.plist file
     //but we want to test with an app that's actually on the store
-    [iVersion sharedInstance].applicationBundleID = @"com.charcoaldesign.rainbowblocks-free";
+    //[iVersion sharedInstance].applicationBundleID = @"com.charcoaldesign.rainbowblocks-free";
+    [iVersion sharedInstance].applicationBundleID = @"com.onethreeeightcom.ios.container";
+    [iVersion sharedInstance].appStoreCountry = @"GB";
     
     //configure iVersion. These paths are optional - if you don't set
     //them, iVersion will just get the release notes from iTunes directly (if your app is on the store)
-    [iVersion sharedInstance].remoteVersionsPlistURL = @"http://charcoaldesign.co.uk/iVersion/versions.plist";
-    [iVersion sharedInstance].localVersionsPlistPath = @"versions.plist";
+//    [iVersion sharedInstance].remoteVersionsPlistURL = @"http://charcoaldesign.co.uk/iVersion/versions.plist";
+//    [iVersion sharedInstance].localVersionsPlistPath = @"versions.plist";
 }
 
 - (BOOL)application:(__unused UIApplication *)application didFinishLaunchingWithOptions:(__unused NSDictionary *)launchOptions
